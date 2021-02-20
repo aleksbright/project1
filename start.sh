8b2219b7ca52a4faebecf2fcc130719a486d1439
aleksbright <djromashko@gmail.com>
2021-02-20 05:50:55 +0000

    third commit

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ crontab -l > current_cron
 sed '/---script managed section---/q' current_cron > new_cron
 cat >> new_cron << EOF
 # updated cron jobs here
-* * * * * make all /home/project1/
+* * * * * cd /home/project1 && make
 EOF
 crontab < new_cron
 rm -f new_cron current_cron
